277e281b4c6436577040514e0c0eb1b7fad49f60
Priyadharshini976 <priyadharshinib1337.sse@saveetha.com>
2025-07-04 18:47:11 +0530

    Create register.php

---
+++ Exp 9/register.php
@@ -0,0 +1,26 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "shopping";
+
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "INSERT INTO users (name, email, username, password) VALUES ('$name', '$email', '$username', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Registration successful!";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
